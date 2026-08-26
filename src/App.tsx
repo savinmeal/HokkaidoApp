15cd6686706bb280a5b44505a80901035c236153
JEFF.C.CHEN 陳準 <JEFF.C.CHEN@deltaww.com>
2026-08-26 10:59:00 +0800

    Initial React travel app

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>北海道之旅</h1>
+
+      <p>2026/12/25 - 2027/01/02</p>
+
+      <h2>Day 1</h2>
+
+      <ul>
+        <li>09:00 新千歲機場</li>
+        <li>11:00 取車</li>
+        <li>13:00 午餐</li>
+        <li>15:00 景點</li>
+        <li>18:00 飯店 Check-in</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
+
